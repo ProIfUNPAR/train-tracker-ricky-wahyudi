785a0380b9e622d6842eee3fbf01c3fcdf5dc805
rickywahyudi <doomknight93@yahoo.co.id>
2018-02-15 09:33:17 +0700

    deploy

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NavParams } from 'ionic-angular/navigation/nav-params';
+
+declare var google: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,35 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('map') mapRef: ElementRef;
 
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  }
+
+  ionViewDidLoad() {
+    this.DisplayMap();
+  }
+
+  DisplayMap() {
+    const location = new google.maps.LatLng('-6.917464', '107.619123');
+
+    const options = {
+      center: location,
+      zoom: 10,
+      streetViewControl: false
+    };
+
+    const map = new google.maps.Map(this.mapRef.nativeElement, options);
+
+    this.addMarker(location, map);
+  }
+
+  addMarker(position, map) {
+    return new google.maps.Marker({
+      position,
+      map
+    });
   }
 
 }
