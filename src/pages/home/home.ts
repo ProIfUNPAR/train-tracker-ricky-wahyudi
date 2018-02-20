dd2d5f56182a52855507e02d1ac7af6b045d9326
rickywahyudi <doomknight93@yahoo.co.id>
2018-02-21 00:44:32 +0700

    current loc

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,44 +1,58 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { NavParams } from 'ionic-angular/navigation/nav-params';
+import { Geolocation } from '@ionic-native/geolocation'; 
 
 declare var google: any;
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  resp: any;
+  map: any;
+  gmLocation: {lat: number, lng: number} = {lat: -6.917464, lng: 107.619123};
   @ViewChild('map') mapRef: ElementRef;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-
+  
+	
+  constructor(public navCtrl: NavController, public navParams: NavParams,private geoloc: Geolocation) {
+	
   }
 
   ionViewDidLoad() {
-    this.DisplayMap();
+    this.onLocateUser();
+	//this.DisplayMap();
+	
+	
   }
 
-  DisplayMap() {
-    const location = new google.maps.LatLng('-6.917464', '107.619123');
+  onLocateUser() {
+    this.geoloc.getCurrentPosition()
+      .then(
+        (resp) => {
+          console.log('position gotten: long:',resp.coords.longitude,' lat:',resp.coords.latitude);
+          this.resp = location;
+          this.gmLocation.lat = resp.coords.latitude;
+          this.gmLocation.lng = resp.coords.longitude;
+		  const options = {center: this.gmLocation,zoom: 15,streetViewControl: false};
+			this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+			const loc = new google.maps.LatLng(this.gmLocation.lat, this.gmLocation.lng);
+			this.addMarker(loc,this.map);
+        }
+      )
+      .catch(
+        (error) => {
+          console.log('Error getting location', error);
+        }
+      )
 
-    const options = {
-      center: location,
-      zoom: 10,
-      streetViewControl: false
-    };
-
-    const map = new google.maps.Map(this.mapRef.nativeElement, options);
-
-    this.addMarker(location, map);
   }
-
+  
+  
   addMarker(position, map) {
-    return new google.maps.Marker({
-      position,
-      map
+   return new google.maps.Marker({
+     position,
+     map
     });
   }
-
 }
